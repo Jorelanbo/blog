ce4c4ef2951992ca252c40ac691e4d0a2ed1e5d6
Jorelanbo <1062976570@qq.com>
2017-06-05 18:22:53 +0800

    去掉了之前的布局初始样式，添加了新的样式

--- life.php
+++ life.php
@@ -35,20 +35,40 @@ require_once 'header.php';
         </div>
 
         <div class="announcement">
-            公告
+            <div class="menu_title">公告</div>
+            <div class="announcement_box">
+                假如生活欺骗了你，<br>
+                不要悲伤，不要心急！<br>
+                忧郁的日子里须要镇静：<br>
+                相信吧，快乐的日子将会来临！<br>
+                心儿永远向往着未来；<br>
+                现在却常是忧郁。<br>
+                一切都是瞬息，一切都将会过去；<br>
+                而那过去了的，就会成为亲切的怀恋。<br>
+                ————普希金《假如生活欺骗了你》
+
+            </div>
         </div>
 
         <div class="newest">
-            最新文章
+            <div class="menu_title">最新文章</div>
         </div>
 
         <div class="scan_times_list">
-            浏览次数排行
+            <div class="menu_title">浏览次数排行</div>
         </div>
 
         <div class="Jorelanbo">
-            Jorelanbo
+            <div class="Jorelanbo_box">
+                <h2>Jorelanbo</h2><br>
+                学习交流。<br>
+                QQ：651118767<br>
+                微信：thegreatGatesby<br>
+                email：Jorelanbo@gmail.com
+            </div>
         </div>
+
+        <!--计划添加悬浮简介-->
     </div>
 </div>
 

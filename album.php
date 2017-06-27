4589c044d8987d90a8e67179d4ce658885dc8588
Jorelanbo <1062976570@qq.com>
2017-06-27 18:30:18 +0800

    添加图片查看器组件，后面可能要换其他的

--- album.php
+++ album.php
@@ -12,8 +12,44 @@
 <head>
     <meta charset="UTF-8">
     <title>John Rainbow</title>
+    <link rel="stylesheet" href="templates/style/viewer.min.css">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        #dowebok {
+            width: 700px;
+            margin: 0 auto;
+            font-size: 0;
+        }
+
+        #dowebok li {
+            display: inline-block;
+            width: 32%;
+            margin-left: 1%;
+            padding-top: 1%;
+        }
+
+        #dowebok li img {
+            width: 100%;
+        }
+    </style>
 </head>
 <body>
-To be finished...
+<ul id="dowebok">
+    <li><img data-original="admin/templates/images/big_bg1.jpg" src="admin/templates/images/big_bg1.jpg" alt="图片1"></li>
+    <li><img data-original="admin/templates/images/big_bg2.jpg" src="admin/templates/images/big_bg2.jpg" alt="图片2"></li>
+    <li><img data-original="admin/templates/images/big_bg3.jpg" src="admin/templates/images/big_bg3.jpg" alt="图片3"></li>
+    <li><img data-original="admin/templates/images/big_bg4.jpg" src="admin/templates/images/big_bg4.jpg" alt="图片4"></li>
+    <li><img data-original="admin/templates/images/big_bg5.jpg" src="admin/templates/images/big_bg5.jpg" alt="图片5"></li>
+</ul>
+<script src="templates/js/viewer.min.js"></script>
+<script>
+    var viewer = new Viewer(document.getElementById('dowebok'), {
+        url: 'data-original'
+    });
+</script>
 </body>
 </html>
